8b8ac3f6e2ddaa1c41bfaf9082f1597bcb202797
beshoynasryz <beshoynasry1@gmail.com>
2024-12-03 03:09:52 +0200

    Appointment

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 const Appointments = () => {
+  const {docId} = useParams()
+  const {doctors} = useContext(appContext)
   return (
     <div>
-      Appointments
+    
     </div>
   )
 }
